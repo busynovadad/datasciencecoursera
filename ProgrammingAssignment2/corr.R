b4006f14ca718f22e9e8b5c3a967492014e910db
Brad McKuhen <bmckuhen@yahoo.com>
2019-09-05 20:33:32 -0400

    working on assign 3 for r class
    Horked it up, so checking in before rebuilding

--- ProgrammingAssignment2/corr.R
+++ ProgrammingAssignment2/corr.R
@@ -4,20 +4,30 @@ corr <- function(
   threshold = 0
 )
 {
-  print(threshold)
+  #print(threshold)
   looplist<-complete(directory)
   
 for (i in looplist$id) 
 {
+  
    #print(i)
   # if key val is above threshold, then do calc
   # else skip
-  paste("For the var i", i, " the value was ", looplist[i,"nobs"])
+  #paste("For the var i", i, " the value was ", looplist[i,"nobs"])
   if (looplist[i,"nobs"] > threshold)
   {
-    #sprintf("For the var i %d the value was %d", i, looplist[i,"nobs"])
+    y<-cor(looplist[i,"nobs"],looplist[i,"nobs"])
+    print(y)
+    if (exists("x")){
+      x<-append(x,y)
+    } else {
+      x<-y
+    }    #sprintf("For the var i %d the value was %d", i, looplist[i,"nobs"])
+    myString<-c("For the var i", i, " the value was ", looplist[i,"nobs"]) #," which is over the threshold of ",threshold)
+    print(myString, quote=F)
   }
 }
-  head(looplist)
+  # head(looplist)
+  head(x)
 }
 
