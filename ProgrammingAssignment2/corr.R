84b77f38021c488c29e87da47c56c87d1c542bce
Brad McKuhen <bmckuhen@yahoo.com>
2019-09-07 11:58:58 -0400

    added all functions to corr.r for homework

--- ProgrammingAssignment2/corr.R
+++ ProgrammingAssignment2/corr.R
@@ -1,7 +1,27 @@
 #source("complete.R")
+
+complete <- function(
+  directory,
+  id = 1:332
+)
+{
+  setwd(directory)
+  files<-list.files(path=getwd(),pattern="*.csv",full.names = T)
+  df<-do.call(rbind, lapply(files, read.csv))
+  dfout = data.frame()
+  for(i in id)
+  {
+    cnt<-nrow(subset(df,df$ID==i & !is.na(df$sulfate) & !is.na(df$nitrate)))
+    dfout <- rbind(dfout, c(i, cnt))
+  }
+  colnames(dfout)<-c("id","nobs")
+  return(dfout)
+}
+
 getsulfate <- function(id)
 {
-  #c<-nchar(id)
+  # based on whether or not the file name is a certain length, 
+  #   then make a decision on how many zero's to prepend
   if (id < 10){
     p<-paste("00",id, collapse = "", sep = "")
   } else if (id > 10 && id < 100) {
@@ -9,18 +29,32 @@ getsulfate <- function(id)
   } else {
     p<-paste("",id, collapse = "", sep = "")
   }
-  #cofp<-nchar(p)
-  #n<-substr(p,(cofp-c),cofp)
-  #print(c)
-  #print(p)
-  #print(cofp)
-  #print(n)
+
   myfilename<-paste(p,".csv", collapse = "", sep = "")
-  #myfilename<-paste(id,".csv")
   myfilename
-  #df<-read.csv(myfilename)
-  #head(df)
+  df<-read.csv(myfilename)
+  head(df)
+
+  df[,"sulfate"]
+  # # another way of subsetting
+  # subset(df,select==c("sulfate"))
+}
+
+getnitrate <- function(id)
+{
+  # based on whether or not the file name is a certain length, 
+  #   then make a decision on how many zero's to prepend
+  if (id < 10){
+    p<-paste("00",id, collapse = "", sep = "")
+  } else if (id > 10 && id < 100) {
+    p<-paste("0",id, collapse = "", sep = "")
+  } else {
+    p<-paste("",id, collapse = "", sep = "")
+  }
   
+  myfilename<-paste(p,".csv", collapse = "", sep = "")
+  df<-read.csv(myfilename)
+  df[,"nitrate"]
 }
 
 corr <- function(
@@ -28,25 +62,26 @@ corr <- function(
   threshold = 0
 )
 {
-  #print(threshold)
   looplist<-complete(directory)
   
 for (i in looplist$id) 
 {
   
-   #print(i)
   # if key val is above threshold, then do calc
   # else skip
   #paste("For the var i", i, " the value was ", looplist[i,"nobs"])
   if (looplist[i,"nobs"] > threshold)
   {
-    y<-cor(looplist[i,"nobs"],looplist[i,"nobs"])
-    print(y)
-    if (exists("x")){
-      x<-append(x,y)
+    x<-getnitrate(i)
+    y<-getsulfate(i)
+    z<-cor(x,y,use="complete.obs")
+    #print(y)
+    if (exists("v")){
+      v<-append(v,z)
     } else {
-      x<-y
-    }    #sprintf("For the var i %d the value was %d", i, looplist[i,"nobs"])
+      v<-z
+    }    
+    #sprintf("For the var i %d the value was %d", i, looplist[i,"nobs"])
     myString<-c("For the var i", i, " the value was ", looplist[i,"nobs"]) #," which is over the threshold of ",threshold)
     print(myString, quote=F)
   }
