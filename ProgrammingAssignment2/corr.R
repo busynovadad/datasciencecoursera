909146e1d36b6f9f37e1ddc271955951c17450ed
Brad McKuhen <bmckuhen@yahoo.com>
2019-09-08 09:51:35 -0400

    finished corr.R
    Finished corr.R; added all functions to single file; added test cases as file

--- ProgrammingAssignment2/corr.R
+++ ProgrammingAssignment2/corr.R
@@ -24,7 +24,7 @@ getsulfate <- function(id)
   #   then make a decision on how many zero's to prepend
   if (id < 10){
     p<-paste("00",id, collapse = "", sep = "")
-  } else if (id > 10 && id < 100) {
+  } else if (id >= 10 && id < 100) {
     p<-paste("0",id, collapse = "", sep = "")
   } else {
     p<-paste("",id, collapse = "", sep = "")
@@ -33,7 +33,7 @@ getsulfate <- function(id)
   myfilename<-paste(p,".csv", collapse = "", sep = "")
   myfilename
   df<-read.csv(myfilename)
-  head(df)
+  #head(df)
 
   df[,"sulfate"]
   # # another way of subsetting
@@ -46,7 +46,7 @@ getnitrate <- function(id)
   #   then make a decision on how many zero's to prepend
   if (id < 10){
     p<-paste("00",id, collapse = "", sep = "")
-  } else if (id > 10 && id < 100) {
+  } else if (id >= 10 && id < 100) {
     p<-paste("0",id, collapse = "", sep = "")
   } else {
     p<-paste("",id, collapse = "", sep = "")
@@ -63,30 +63,37 @@ corr <- function(
 )
 {
   looplist<-complete(directory)
-  
+  v<-numeric()
 for (i in looplist$id) 
 {
   
   # if key val is above threshold, then do calc
   # else skip
   #paste("For the var i", i, " the value was ", looplist[i,"nobs"])
+  #print(i)
+  #print(looplist[i,"nobs"])
   if (looplist[i,"nobs"] > threshold)
   {
+    #print("getting nitrate")
     x<-getnitrate(i)
+    #print("getting sulfate")
     y<-getsulfate(i)
+    #print("getting correlation")
     z<-cor(x,y,use="complete.obs")
-    #print(y)
-    if (exists("v")){
+    #print(z)
+    #exists("v")
+    # if (exists("v")){
       v<-append(v,z)
-    } else {
-      v<-z
-    }    
+    # } else {
+    #   v<-z
+    # }    
     #sprintf("For the var i %d the value was %d", i, looplist[i,"nobs"])
     myString<-c("For the var i", i, " the value was ", looplist[i,"nobs"]) #," which is over the threshold of ",threshold)
-    print(myString, quote=F)
+    #print(myString, quote=F)
   }
 }
   # head(looplist)
-  head(x)
+  #head(v)
+  v
 }
 
